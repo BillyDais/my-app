78a988d58aa78b4351c134a1342f67402b8ab10b
Vasilis Dais <vdais@Vasiliss-MacBook-Pro.local>
2019-06-06 14:34:17 -0400

    FUTURE

--- src/App.js
+++ src/App.js
@@ -8,11 +8,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Future Hendrixx is dropping an album tonight!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://hypebeast.com/2019/6/future-save-me-album-announcement-release-friday"
           target="_blank"
           rel="noopener noreferrer"
         >
